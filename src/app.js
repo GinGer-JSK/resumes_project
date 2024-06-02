fb1785cb5ee002db73aaa8dff9819cf9d5b5a09d
GinGer-JSK <jskim5701@gmail.com>
2024-06-02 18:13:54 +0900

    프로젝트 초기 세팅 완료

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { SERVER_PORT } from './constants/env.constant.js';
+import { HTTP_STATUS } from './constants/http-status.constant.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/health-check', (req, res) => {
+  return res.status(HTTP_STATUS.OK).send(`I'm healthy.`);
+});
+console.log(SERVER_PORT);
+app.listen(SERVER_PORT, () => {
+  console.log(`서버가 ${SERVER_PORT}번 포트에서 실행 중입니다.`);
+});
---
+++ src/constants/env.constant.js
@@ -0,0 +1,5 @@
+import "dotenv/config";
+export const SERVER_PORT = process.env.SERVER_PORT;
+
+console.log(process.env.SERVER_PORT);
+console.log(SERVER_PORT);
